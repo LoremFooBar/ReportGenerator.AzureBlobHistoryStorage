fc4db6493aa70ac04eb4eb3beea5fff2bd7f939e
LoremFooBar <loremfoobardev@gmail.com>
2022-07-24 20:44:13 -0400

    fix: read DEBUG environment variable

--- src/ReportGenerator.AzureBlobHistoryStorage/EnvironmentVariablesOptionsReader.cs
+++ src/ReportGenerator.AzureBlobHistoryStorage/EnvironmentVariablesOptionsReader.cs
@@ -4,7 +4,7 @@ public static class EnvironmentVariablesOptionsReader
 {
     public static Dictionary<string, string> ReadOptionsFromEnvironment()
     {
-        string[] variables = { "HISTORYCONTAINERURL", "WRITESASTOKEN", "REPOSITORYNAME" };
+        string[] variables = { "HISTORYCONTAINERURL", "WRITESASTOKEN", "REPOSITORYNAME", "DEBUG" };
         var res = new Dictionary<string, string>();
 
         foreach (string variable in variables) {
